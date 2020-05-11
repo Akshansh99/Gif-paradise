b5e6e10d737a82db8a5645880d0959b247707f5f
kra-tosss <64043098+kra-tosss@users.noreply.github.com>
2020-05-11 20:29:02 +0530

    add login , register and landing page

--- app.js
+++ app.js
@@ -4,9 +4,25 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+
+//Landing page 
 app.get("/", (req, res) => {
-    res.send("Home Route Working");
+    res.render("landing");
 });
+
+//register page
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+
+//Login page
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+
+
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server Started Successfully at :3000');
